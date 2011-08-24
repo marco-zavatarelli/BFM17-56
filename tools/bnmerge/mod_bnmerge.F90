720e5bc2f4e4521863ac9b2222e89c0cc6c653fc
Marcello Vichi <marcello.vichi@bo.ingv.it>
2011-08-24 15:28:32 +0200

    Added directory $BFMDIR/tools and the bnmerge tool
    BNMERGE is a tool to merge the output files produced by BFM_NEMO when run
    in parallel. It is controlled by the namelist bnmerge.nml through the
    following parameters:
    out_fname       string  Name of NetCDF output files "out_fname_????.nc"
    out_dir         string  Path to the output files
    layout          string  Path to the layout.dat file
    old_layout_flag logical If true reads layout.dat prior to NEMO V3
    as for instance
    &bfm_nml
       out_fname='PELAGOS_ORCA2'
       out_dir='/path/to/output'
       layout = '/path/to/layout.dat'
       old_layout_flag = .FALSE.
    /
    The output files are remapped from the 1D BFM grid to the 3D NEMO grid.
    All variables are copied and the output file contains also the Tmask and
    lat lon arrays.

---
+++ tools/bnmerge/mod_bnmerge.F90
@@ -0,0 +1,70 @@
+! BFM_NEMO-MERGE bnmerge
+!    Copyright (C) 2009-2011 Marcello Vichi (marcello.vichi@bo.ingv.it)
+!
+!    This program is free software: you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    (at your option) any later version.
+!
+!    This program is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
+!
+!    You should have received a copy of the GNU General Public License
+!    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+module mod_bnmerge
+
+  integer,    parameter, public :: RLEN=selected_real_kind(12,307)
+  real(RLEN), parameter, public :: ZERO=0.0_RLEN
+  real(RLEN), parameter, public :: ONE=1.0_RLEN
+
+   integer, public ::               &  
+      jpi       ,                   &  !: number of grid points along i (proc)
+      jpj       ,                   &  !: number of grid points along j (proc)
+      jpk       ,                   &  !: number of grid points along k (proc)
+      jpiglo    ,                   &  !: number of grid points along i (global domain)
+      jpjglo    ,                   &  !: number of grid points along j (global domain)
+      jpni      ,                   &  !: number of processors following i 
+      jpnj      ,                   &  !: number of processors following j
+      jpnij                            !: nb of local domain = nb of processors 
+      !                                !  ( <= jpni x jpnj )
+
+   integer, public, allocatable, dimension(:) ::   &  !:
+      nimppt, njmppt,  &  !: i-, j-indexes for each processor
+      ibonit, ibonjt,  &  !: i-, j- processor neighbour existence
+      nlcit , nlcjt,   &  !: dimensions of every subdomain
+      nldit , nldjt,   &  !: first, last indoor index for each i-domain
+      nleit , nlejt       !: first, last indoor index for each j-domain
+
+   ! masks
+   real, public, allocatable, dimension(:,:)                 :: latglo, longlo ! FLOAT
+   real(RLEN), public, allocatable, target, dimension(:,:,:) :: maskglo
+
+   ! no. of ocean points in the whole domain (volume and surface)
+   integer,public           :: ocepoints, srfpoints
+
+   ! NetCDF IDs  of variables to be merged
+   integer,public                                  :: n_bfmvar
+   integer,allocatable,dimension(:),public         :: bfmvarid
+
+   character(LEN=100) :: out_dir, out_fname
+   logical            :: old_layout_flag = .FALSE.
+   public 
+   contains 
+
+   subroutine handle_err(iret,errstring)
+   use netcdf
+   implicit none
+     integer,intent(in)  :: iret
+     character(len=*),optional,intent(in) :: errstring
+     if (iret .ne. NF90_NOERR) then
+       write(*,*) "====== NetCDF Error ======"
+       if (present(errstring)) write(*,*) errstring
+       write(*,*) NF90_STRERROR(iret)
+       stop "stop in function handle_err"
+     endif
+   end subroutine handle_err
+   
+end module mod_bnmerge
---
+++ tools/bnmerge/read_input.F90
@@ -0,0 +1,78 @@
+! BFM_NEMO-MERGE bnmerge
+!    Copyright (C) 2009-2011 Marcello Vichi (marcello.vichi@bo.ingv.it)
+!
+!    This program is free software: you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    (at your option) any later version.
+!
+!    This program is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
+!
+!    You should have received a copy of the GNU General Public License
+!    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+subroutine read_input
+
+  use mod_bnmerge
+  integer,parameter :: inum=199
+  integer           :: iost,n
+  character(LEN=120) :: dummyline,layout
+  integer,parameter    :: namlst=10,unit=11
+  namelist /bnmerge_nml/ out_dir,out_fname,layout,old_layout_flag 
+
+
+      ! Reading directory names and file name specification
+      open(namlst,file='bnmerge.nml',action='read',status='old',err=99)
+      read(namlst,nml=bnmerge_nml,err=98)
+      close(namlst)
+      ! read processor layout from layout.dat file 
+      open ( UNIT=inum, FILE=layout, FORM='FORMATTED', ACCESS='SEQUENTIAL',   &
+               STATUS='UNKNOWN', ERR=100, IOSTAT=iost)
+      if ( iost == 0 ) then
+               write (*,*) '     file   = ', trim(layout),' opened correctly'
+               write (*,*) '     unit   = ', inum
+               write (*,*)
+      end if
+      if ( .NOT. old_layout_flag) read (inum,'(a)') dummyline
+      read (inum,'(6i8)') jpnij,jpi,jpj,jpk,jpiglo,jpjglo
+      read (inum,'(a)') dummyline
+      allocate (nimppt(jpnij))
+      allocate (njmppt(jpnij))
+      allocate (ibonit(jpnij))
+      allocate (ibonjt(jpnij))
+      allocate (nlcit(jpnij))
+      allocate (nlcjt(jpnij))
+      allocate (nldit(jpnij))
+      allocate (nldjt(jpnij))
+      allocate (nleit(jpnij))
+      allocate (nlejt(jpnij))
+      do n = 1, jpnij
+         read (inum,'(9i5)') jn, nlcit(jn), nlcjt(jn), &
+                             nldit(jn), nldjt(jn), &
+                             nleit(jn), nlejt(jn), &
+                             nimppt(jn), njmppt(jn)
+      end do
+      close(inum)
+      write (*,*) ' === ',trim(layout),' === '
+      write (*,'(a)') '   jpnij     jpi     jpj     jpk  jpiglo  jpjglo'
+      write (*,'(6i8)') jpnij,jpi,jpj,jpk,jpiglo,jpjglo
+      return
+
+100   continue
+      if ( iost /= 0 ) then
+          write (*,*)
+          write (*,*) ' ===>>>> : problem opening file: ', trim(layout)
+          write (*,*) ' =======   ===  '
+          write (*,*) '           unit   = ', inum
+          write (*,*) '           iostat = ', iost
+          write (*,*) '           verify the file '
+          write (*,*)
+          stop 'Stop in read_input'
+      endif
+99    stop 'I could not open bnmerge.nml'
+98    stop 'I could not read bnmerge_nml'
+
+end subroutine read_input
