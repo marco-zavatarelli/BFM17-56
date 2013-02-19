56cd0228c1f9aaebfc3f025a3447ebd5c1c72d59
Marcello Vichi <marcello.vichi@bo.ingv.it>
2013-02-19 11:47:19 +0100

    Check consistency of bacteria parameters
    The bacteria parameters are checked depending on the chosen
    parameterization (p_version). A warning is raised and the
    parameters are modified.

--- src/BFM/Pel/ModulePelBac.F90
+++ src/BFM/Pel/ModulePelBac.F90
@@ -6,38 +6,30 @@
 ! !ROUTINE: PelBac
 !
 ! DESCRIPTION
-!   Here are the values for the pelagic bacteria.
-!
-!
-
-!   This file is generated directly from OpenSesame model code, using a code 
-!   generator which transposes from the sesame meta language into F90.
-!   F90 code generator written by P. Ruardij.
-!   structure of the code based on ideas of M. Vichi.
+!   Module containing the parameters for bacterioplankton and the 
+!   initialization and consistency check
 !
 ! !INTERFACE
   module mem_PelBac
 !
 ! !USES:
-
   use global_mem
-
 !  
 !
 ! !AUTHORS
-!   Original version by J.W. Baretta and Piet Ruardij
-!
-!
+!   First ERSEM version by J.W. Baretta and H. Baretta-Bekker
+!   Additional parametrizations by P. Ruardij and M. Vichi 
+!   (Vichi et al., 2004; Vichi et al., 2007)
+!   L. Polimene, I. Allen and M. Zavatarelli (Polimene et al., 2006)
+!   Dynamical allocation by G. Mattia 
 !
 ! !REVISION_HISTORY
-!   2006-03-17 Piet Ruardij : density dependent mortality
-!  rua: density dependent mortality set on zero now!!
-!
-!
+!   !
 !
 ! COPYING
 !   
-!   Copyright (C) 2006 P. Ruardij, the mfstep group, the ERSEM team 
+!   Copyright (C) 2013 BFM System Team (bfm_st@lists.cmcc.it)
+!   Copyright (C) 2006 P. Ruardij and M. Vichi
 !   (rua@nioz.nl, vichi@bo.ingv.it)
 !
 !   This program is free software; you can redistribute it and/or modify
@@ -64,60 +56,82 @@
   !-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   ! PelBac PARAMETERS (read from nml)
   !-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-  integer  :: p_version  ! Switch for DOM uptake parameterization
-  !  p_version=1 <LUCA> Polimenes version
-  !  p_version=2 <BFM> version
-  real(RLEN)  :: p_q10  ! Q10-value (temperature dependency)
-  real(RLEN)  :: p_chdo  ! Michaelis const for O2 dependence (mmol/m3)
-  real(RLEN)  :: p_sd  ! Independent specific mortality (1/d)
-  real(RLEN)  :: p_sd2  ! Density dependent mortality (value: 0.009) (1/d)
-  real(RLEN)  :: p_suhR1  ! Specific potential of rich DOM availability (1/d)
-  real(RLEN)  :: p_sulR1  ! Specific potential sugar availability (1/d)
-  real(RLEN)  :: p_suR2  ! Specific potential TEP availability (1/d)
-  real(RLEN)  :: p_suR6  ! Availability of POM (1/d)
-  real(RLEN)  :: p_suR7  ! Availability of semi-refractory DOC (1/d)
-  real(RLEN)  :: p_sum  ! Specific potential uptake (1/d)
-  real(RLEN)  :: p_pu_ra  ! Activity respiration (-)
+  integer     :: p_version  ! Switch for DOM uptake parameterization
+  real(RLEN)  :: p_q10      ! Q10-value (temperature dependency)
+  real(RLEN)  :: p_chdo     ! Michaelis const for O2 dependence (mmol/m3)
+  real(RLEN)  :: p_sd       ! Independent specific mortality (1/d)
+  real(RLEN)  :: p_sd2      ! Density dependent mortality (value: 0.009) (1/d)
+  real(RLEN)  :: p_suhR1    ! Specific potential of rich DOM availability (1/d)
+  real(RLEN)  :: p_sulR1    ! Specific potential sugar availability (1/d)
+  real(RLEN)  :: p_suR2     ! Specific potential TEP availability (1/d)
+  real(RLEN)  :: p_suR6     ! Availability of POM (1/d)
+  real(RLEN)  :: p_suR7     ! Availability of semi-refractory DOC (1/d)
+  real(RLEN)  :: p_sum      ! Specific potential uptake (1/d)
+  real(RLEN)  :: p_pu_ra    ! Activity respiration (-)
   real(RLEN)  :: p_pu_ra_o  ! Decrease in Ass. efficiency at low O2 conc (-).
-  real(RLEN)  :: p_srs  ! Specific rest respiration (1/day)
-  real(RLEN)  :: p_qnc  ! Optimal N/C ratio (model units) 45:9:1
-  real(RLEN)  :: p_qpc  ! Optimal P/C ratio (model units) C:N:P
-  real(RLEN)  :: p_qlnc  ! Minimal N/C ratio (model units) 45:9:1 <BFM>
-  real(RLEN)  :: p_qlpc  ! Minimal P/C ratio (model units) C:N:P (BFM>
-  real(RLEN)  :: p_qun  ! nutrient affinity ( mmol/mgC/day) <BFM>
-  real(RLEN)  :: p_qup  ! nutrient affinity ( mmol/mgC/day) <BFM>
-  real(RLEN)  :: p_lN4  ! ammonium conc. at which nutrate uptake are equal (BFM)
-  real(RLEN)  :: p_chn  ! half saturation ammonium conc. for uptake
-  real(RLEN)  :: p_chp  ! half saturation phosphate conc. for uptake
-  real(RLEN)  :: p_pu_ea_R7  ! excretion of reg. org. met. (-) <LUCA>
+  real(RLEN)  :: p_srs      ! Specific rest respiration (1/day)
+  real(RLEN)  :: p_qnc      ! Optimal N/C ratio (model units) 45:9:1
+  real(RLEN)  :: p_qpc      ! Optimal P/C ratio (model units) C:N:P
+  real(RLEN)  :: p_qlnc     ! Minimal N/C ratio (model units) 45:9:1 
+  real(RLEN)  :: p_qlpc     ! Minimal P/C ratio (model units) C:N:P 
+  real(RLEN)  :: p_qun      ! nutrient affinity ( mmol/mgC/day) 
+  real(RLEN)  :: p_qup      ! nutrient affinity ( mmol/mgC/day) 
+  real(RLEN)  :: p_lN4      ! ammonium conc. at which nitrate uptake are equal
+  real(RLEN)  :: p_chn      ! half saturation ammonium conc. for uptake
+  real(RLEN)  :: p_chp      ! half saturation phosphate conc. for uptake
+  real(RLEN)  :: p_pu_ea_R7 ! excretion of semirefractory DOC (-) 
+
   !-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   ! SHARED PUBLIC FUNCTIONS (must be explicited below "contains")
-
   public InitPelBac
+
   contains
 
   !-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  ! Initialization of Pelagic Bacteria
+  ! Definition of th enamelist with all the parameters
+  !-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   subroutine InitPelBac()
-
   !-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   namelist /PelBac_parameters/ p_version, p_q10, p_chdo, p_sd, p_sd2, p_suhR1, &
     p_sulR1, p_suR2, p_suR6, p_sum, p_pu_ra, p_pu_ra_o, p_pu_ea_R7, p_srs, &
     p_suR7, p_qpc, p_qlpc, p_qnc, p_qlnc, p_qun, p_qup, p_lN4, p_chn, p_chp
-  !-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
+  !-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   !BEGIN compute
   !-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
   !  Open the namelist file(s)
   !-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  write(LOGUNIT,*) "#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+  write(LOGUNIT,*) "#  Reading PelBac parameters.."
+  open(NMLUNIT,file='Pelagic_Ecology.nml',status='old',action='read',err=100)
+  read(NMLUNIT,nml=PelBac_parameters,err=101)
+  close(NMLUNIT)
 
-write(LOGUNIT,*) "#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
-   write(LOGUNIT,*) "#  Reading PelBac parameters.."
-open(NMLUNIT,file='Pelagic_Ecology.nml',status='old',action='read',err=100)
-    read(NMLUNIT,nml=PelBac_parameters,err=101)
-    close(NMLUNIT)
-    write(LOGUNIT,*) "#  Namelist is:"
-    write(LOGUNIT,nml=PelBac_parameters)
+  !-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  ! Check consistency of parameters according to the parametrization
+  !-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  write(LOGUNIT,*) "#  using p_version=",p_version
+  select case ( p_version )
+    case ( 1 ) ! Polimene et al. (2006)
+      p_sulR1 = ZERO
+      write(LOGUNIT,*) "#  forcing p_sulR1=0"
+    case ( 2 ) ! Vichi et al. 2007
+      p_sulR1 = ZERO
+      p_suR2 = ZERO
+      p_suR7 = ZERO
+      write(LOGUNIT,*) "#  forcing p_sulR1,p_suR2,p_suR7=0"
+    case ( 3 ) ! Vichi et al. 2004
+      p_suR7 = ZERO
+      write(LOGUNIT,*) "#  forcing p_suR7=0"
+  end select
+
+  !-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  ! Write parameter list to the log
+  !-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  write(LOGUNIT,*) "#  Namelist is:"
+  write(LOGUNIT,nml=PelBac_parameters)
+
   !-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   !END compute
   !-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
