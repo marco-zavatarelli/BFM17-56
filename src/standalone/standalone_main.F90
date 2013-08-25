0ad9eea59f6efb153bb20bd95679e91accf6c3f3
Marcello Vichi <marcello.vichi@bo.ingv.it>
2013-08-25 11:00:41 +0200

    Remove old declarations or REAL*4
    Some code in the STANDALONE still had simple real declarations that failed
    compilation if option -r8 was removed

--- src/standalone/standalone_main.F90
+++ src/standalone/standalone_main.F90
@@ -22,24 +22,13 @@
 !  Author(s): Momme Butenschoen (UNIBO) and Marcello Vichi (INGV)
 !
 ! !LOCAL VARIABLES:
-#ifdef IFORT
-   real                      :: t1=-1,t2=-1
-#endif
 ! 
 !EOP
 !-----------------------------------------------------------------------
 !BOC
-#ifdef IFORT
-   call CPU_Time(t1)
-#endif
       call init_standalone
       call timestepping
       call end_standalone
-
-#ifdef IFORT
-   call CPU_Time(t2)
-   write(*,*) 'CPU-time was in loop:  ',t2-t1,' seconds'
-#endif
 
       END PROGRAM main
 !EOC
