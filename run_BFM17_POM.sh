4082dd363f6ce11a75272b92d36f7ff3a4fe3630
Tomas Lovato <tomas.lovato@cmcc.it>
2020-05-20 12:17:29 +0200

    adjust runscript for BFM_POM

--- run_BFM17_POM.sh
+++ run_BFM17_POM.sh
@@ -6,21 +6,30 @@ export BFMDIR="$(pwd)"
 export NETCDF="/usr/local/netcdf"
 export BFMDIR_RUN="$BFMDIR/bfm_run"
 
-mkdir $BFMDIR_RUN/bfm17_pom1d
+echo "BFMDIR is "${BFMDIR}
+
+mkdir -p $BFMDIR_RUN/bfm17_pom1d
 cd $BFMDIR_RUN/bfm17_pom1d
 rm -rf  pom.exe pom_restart* bfm_restart*
 
 cd $BFMDIR/build
 
-./bfm_configure.sh -gdc -p BFM17_POM1D BFM17
+./bfm_configure.sh -gcd -p BFM17_POM1D BFM17
 
 cd $BFMDIR_RUN/bfm17_pom1d
-cp $BFMDIR/build/configurations/BFM17_POM1D/* .
+cp $BFMDIR/build/configurations/BFM17_POM1D/*.nml .
 
-ln -s pom_restart_$ID_run fort.70 #in
-ln -s pom_restart_$next_ID_run fort.71 #out
+echo "Namelists copied"
 
-ln -s bfm_restart_$ID_run in_bfm_restart.nc #in
-ln -s bfm_restart_$next_ID_run out_bfm_restart.nc #out
+if [ "x$ID_run" != "x" ] && [ "y$next_ID_run" != "y" ] ; then
+   ln -s pom_restart_$ID_run fort.70 #in
+   ln -s pom_restart_$next_ID_run fort.71 #out
+   
+   ln -s bfm_restart_$ID_run in_bfm_restart.nc #in
+   ln -s bfm_restart_$next_ID_run out_bfm_restart.nc #out
+   echo "restart files linked"
+fi
+
+echo "Start model simulation"
 
 time ./pom.exe
